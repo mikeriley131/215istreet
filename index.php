a968a0b6cbd04fff0357d5fe9193b7c04ddbdc19
Mike Riley <mikeriley131@gmail.com>
2015-03-02 19:25:52 -0500

    Initial file commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="description" content="" />
+
+		<title></title>
+
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" href="css/styles.css" />
+
+		<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+		<!--[if IE]>
+			<script src= »
+			"http://html5shiv.googlecode.com/svn/trunk/html5.js">
+			</script>
+		<![endif]-->
+	</head>
+	<body>
+		
+	</body>
+</html>
